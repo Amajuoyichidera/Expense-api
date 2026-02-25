ec56cb61f513741e41b9f026c4cc256d854595b5
Amajuoyichidera <amajuoyichideradavid@gmail.com>
2026-02-25 15:27:29 +0000

    Feat: updated my CRUD functions to match mongodb standard

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const Expense = require('./models/Expense');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const Expense = require("./models/Expense");
 
 const app = express();
 
@@ -10,73 +10,79 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
-
-app.get('/', (req, res) => {
-    res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 const PORT = 5000;
 
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
-})
+  console.log(`server running on port ${PORT}`);
+});
 
 // to get all expenses
-app.get('/expenses', async (req, res) => {
-    try {
-        const expenses = await Expense.find();
-        res.json(expenses);
-    } catch (error) {
-        res.status(500).json({message: 'Server error'})
-    }
-})
+app.get("/expenses", async (req, res) => {
+  try {
+    const expenses = await Expense.find();
+    res.json(expenses);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 // to add a new expense
-app.post('/expenses', (req, res) => {
+app.post("/expenses", async (req, res) => {
+  try {
     const { title, amount } = req.body;
 
-    if(!title || !amount) {
-        return res.status(400).json({ message: "Title and amount are required"});
+    if (!title || !amount) {
+      return res.status(400).json({ message: "Title and amount are required" });
     }
 
-    const newExpense = {
-        id: expenses.length + 1,
-        title,
-        amount
-    }
+    const newExpense = await Expense.create({
+      title,
+      amount,
+    });
 
-    expenses.push(newExpense);
     res.status(201).json(newExpense);
-})
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 // to delete an expense
-app.delete('/expenses/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-
-    expenses = expenses.filter((expense) => expense?.id !== id);
-    res.status(200).json({ message: 'expense deleted'})
-})
+app.delete("/expenses/:id", async (req, res) => {
+  try {
+    const deletedExpense = await Expense.findByIdAndDelete(req.params.id);
+    if(!deletedExpense) {
+        return res.status(404).json({ message: "Expense not found"});
+    }
+    res.json({message: 'Expense deleted'})
+  } catch (error) {
+    res.status(500).json({ message: 'server error'});
+  }
+});
 
 // to update an expense
-app.put('/expenses/:id', (req, res) => {
-    const id = parseInt(req.params.id);
+app.put("/expenses/:id", async (req, res) => {
+  try {
     const { title, amount } = req.body;
 
-    const updatedExpense = expenses.find(exp => exp?.id === id);
+    const updatedExpense = await Expense.findByIdAndUpdate(
+      req.params.id,
+      { title, amount },
+      { new: true }
+    );
     if (!updatedExpense) {
-        res.status(404).json({ message: 'Expense not found'});
+      res.status(404).json({ message: "Expense not found" });
     }
-
-    if(title) {
-        updatedExpense.title = title
-    }
-
-    if(amount) {
-        updatedExpense.amount = amount
-    }
-
-    res.json(expenses);
-})
+    res.json(updatedExpense);
+  } catch (error) {
+    res.status(500).json({ message: "server error" });
+  }
+});
